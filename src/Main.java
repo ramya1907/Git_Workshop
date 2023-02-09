7f76d806f7209a1fd6eb9edde1e6f557c5f25873
Ramya Srinivasan <ramya.srinivasan1907@gmail.com>
2023-02-09 16:16:41 +0400

    Added a for loop in firstMethod()

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 
+    int firstMethod(){
+        for(int i = 0; i < 5; i++)
+        {
+            System.out.println(i);
+        }
+        return 0;
+    }
     public static void main(String [] args){
 
         System.out.println("Hello World");

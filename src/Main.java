b3ba17e3b0bf1798ec0663806b4916f3a7941e53
Ramya Srinivasan <ramya.srinivasan1907@gmail.com>
2023-02-09 16:18:11 +0400

    Changed for loop in firstMethod()

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     int firstMethod(){
         for(int i = 0; i < 5; i++)
         {
-            System.out.println(i);
+            System.out.println(i + 4);
         }
         return 0;
     }
